7aa8b74c7536c8c2085082f109dde144287d9c54
silvi-hdez <silviahdez.b@gmail.com>
2023-01-05 20:25:34 +0100

    Primeros pasos

--- src/memory.js
+++ src/memory.js
@@ -1,18 +1,31 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
+    this.isFinished = false;
   }
 
-  shuffleCards() {
-    // ... write your code here
-  }
+  shuffleCards() {}
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+    this.pairsClicked += 1;
+    if (card1 === card2) {
+      this.pairsGuessed += 1;
+      return true;
+    } else {
+      return false;
+    }
   }
 
   checkIfFinished() {
-    // ... write your code here
+    if ((this.pickedCards.length = 2)) {
+      this.isFinished = true;
+      return this.isFinished;
+    } else {
+      this.isFinished = false;
+      return this.isFinished;
+    }
   }
 }
